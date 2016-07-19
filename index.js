a373e24a6095a73d33fd1d1a7de412c528195084
Jacob Greenberg <jgreenberg45@yahoo.com>
2016-07-18 20:08:51 -0400

    Implemented hourly forecast url

--- index.js
+++ index.js
@@ -3,14 +3,32 @@ var cors = require('cors');
 var bodyParser = require('body-parser');
 var server = express();
 var port = process.env.PORT || 8080;
+var apiKey = require('./config').apiKey;
+var $http = require('axios');
+var baseUrl = 'https://api.forecast.io/forecast/';
 
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended: true}));
 server.use(cors());
 
-server.get('/', function(req,res)
+server.get('/forecast/hourly/:lat,:lon', function(req,res)
 {
-  res.send('test');
+  $http.get(baseUrl + apiKey + '/' + req.params.lat + ',' + req.params.lon)
+        .then(function(response)
+        {
+          var resObj =
+          {
+            latitude: response.data.latitude,
+            longitude: response.data.longitude,
+            hourly: response.data.hourly,
+          };
+          res.json(resObj);
+        })
+        .catch(function(error)
+        {
+          console.log(error);
+          console.log('catch error');
+        });
 });
 
 server.listen(port, function()
